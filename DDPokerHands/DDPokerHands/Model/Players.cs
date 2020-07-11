8dbf9fee96b1e700e6996eb8044eb51dd3619c8d
DCodeHub <32890071+DCodeHub@users.noreply.github.com>
2020-07-11 22:12:20 +1000

    Added project Files

---
+++ DDPokerHands/DDPokerHands/Model/Players.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDPokerHands.Model
+{
+    public class Players
+
+    {
+        public List<string> Player1 { get; set; }
+        public List<string> Player2 { get; set; }
+    }
+}
